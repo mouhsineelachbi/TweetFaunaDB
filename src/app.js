ea9ef5aa43e2aa4ef49fa1c6ceb95e41a724e2bb
mouhsineelachbi <mouhsineelachbi@gmail.com>
2020-10-15 23:21:39 +0100

    init project with faunadb express and nodemon for dev env

--- src/app.js
+++ src/app.js
@@ -8,6 +8,19 @@ const client = new faunadb.Client({
   secret: process.env.KEY,
 });
 
+const {
+  Pagination,
+  Get,
+  Select,
+  Match,
+  Index,
+  Create,
+  Collection,
+  Lambda,
+  Var,
+  Join,
+} = faunadb.query;
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`API on http://localhost:${PORT}`));
